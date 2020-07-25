32d9e91ba67c16ef4876b4876a4e719dcc77e072
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2020-07-26 02:19:38 +0530

    image

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000
 
+const imageSearch = require('image-search-google');
+
+const client = new imageSearch(process.env.CSE, process.env.apiKey);
+const options = { page: 1 };
+client.search('APJ Abdul kalam', options)
+    .then(images => {
+        console.log(images)
+        /*
+        
+		[{
+			'url': item.link,
+            'thumbnail':item.image.thumbnailLink,
+            'snippet':item.title,
+            'context': item.image.contextLink
+		}]
+		 */
+    })
+    .catch((error) => { console.log(error) });
+
+// search for certain size
+// client.search('Mahatma Gandhi', { size: 'large' });
+
+// search for certain type
+// client.search('Indira Gandhi', { type: 'face' });
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
