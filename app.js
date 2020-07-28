48463a4ecc0cdd1898564a4bff14eb45db539cf0
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2020-07-28 13:47:37 +0530

    edit

--- app.js
+++ app.js
@@ -13,23 +13,10 @@ const options = { page: 1 };
 client.search('APJ Abdul kalam', options)
     .then(images => {
         console.log(images)
-        /*
-        
-		[{
-			'url': item.link,
-            'thumbnail':item.image.thumbnailLink,
-            'snippet':item.title,
-            'context': item.image.contextLink
-		}]
-		 */
     })
     .catch((error) => { console.log(error) });
 
-// search for certain size
-// client.search('Mahatma Gandhi', { size: 'large' });
 
-// search for certain type
-// client.search('Indira Gandhi', { type: 'face' });
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
